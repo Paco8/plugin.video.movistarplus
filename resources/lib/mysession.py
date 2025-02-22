0a4044a6765e402572f65668303f488d82bceeb4
Paco8 <5084042+Paco8@users.noreply.github.com>
2025-02-22 10:33:10 +0100

    Version 0.11.0

--- resources/lib/mysession.py
+++ resources/lib/mysession.py
@@ -5,28 +5,24 @@
 from __future__ import unicode_literals, absolute_import, division
 
 import requests
-
 import ssl
+import sys
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 
 class CustomAdapter(HTTPAdapter, object):
-  try:
-    ctx = ssl.create_default_context()
-    ctx.set_ciphers('DEFAULT@SECLEVEL=1')
-
+  if sys.version_info[0] == 3:
     def init_poolmanager(self, connections, maxsize, block=False):
+      ctx = ssl.create_default_context()
       self.poolmanager = PoolManager(
           num_pools=connections,
           maxsize=maxsize,
           block=block,
-          ssl_version=ssl.PROTOCOL_TLS,
-          ssl_context=self.ctx
+          ssl_context=ctx
       )
-  except:
-    pass
 
 class MySession(requests.Session, object):
   def __init__(self, *args, **kwargs):
     super(MySession, self).__init__(*args, **kwargs)
-    self.mount('https://', CustomAdapter())
+    if sys.platform.startswith("linux") and sys.version_info[0] == 3:
+      self.mount('https://', CustomAdapter())
